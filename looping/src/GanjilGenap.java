58ac4fa8ff37409df5cd67d1bf1b99e162bb6860
alditiya <aldit4@gmail.com>
2021-11-26 17:29:51 +0700

    format doc

--- looping/src/GanjilGenap.java
+++ looping/src/GanjilGenap.java
@@ -1,16 +1,16 @@
 public class GanjilGenap {
-    public static void main(String [] args){
-        //Ganjil
+    public static void main(String[] args) {
+        // Ganjil
         System.out.print("Bilangan Ganjil dari 1 - 30 : " + '\n');
-        for(int x=1; x <=30;x++){
-        if(x % 2 == 1 )
-        System.out.print(" "+x);
+        for (int x = 1; x <= 30; x++) {
+            if (x % 2 == 1)
+                System.out.print(" " + x);
         }
-        //Genap
-        System.out.print('\n'+"Bilangan Genap dari 1 - 30 : " + '\n');
-        for(int x=1; x <=30;x++){
-        if(x % 2 == 0 )
-        System.out.print(" "+x);
+        // Genap
+        System.out.print('\n' + "Bilangan Genap dari 1 - 30 : " + '\n');
+        for (int x = 1; x <= 30; x++) {
+            if (x % 2 == 0)
+                System.out.print(" " + x);
         }
     }
 }
--- looping/src/looping.java
+++ looping/src/looping.java
@@ -1,14 +1,13 @@
 public class looping {
     public static void main(String args[]) {
-// For Loop akan mengeluarkan output print: 1-10
-    for(int i = 1; i<=10; i++){
-    System.out.println("Output Berurut: " + i );
-    }
+        // For Loop akan mengeluarkan output print: 1-10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Output Berurut: " + i);
+        }
 
-    // Ini akan mengeluarkan output print: 10 - 1
-    for(int i = 10; i>=1; i--){
-    System.out.println("Output Terbalik: " + i );
-    }
+        // Ini akan mengeluarkan output print: 10 - 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Output Terbalik: " + i);
+        }
     }
 }
-   
