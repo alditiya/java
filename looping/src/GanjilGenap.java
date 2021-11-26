4225322d8d85a85ae39cb32fdb024ab2b0e1ee14
alditiya <aldit4@gmail.com>
2021-11-26 12:14:00 +0700

    modifikasi gage

--- looping/src/GanjilGenap.java
+++ looping/src/GanjilGenap.java
@@ -1,13 +1,13 @@
 public class GanjilGenap {
     public static void main(String [] args){
         //Ganjil
-        System.out.print("Bilangan Ganjil dari 1 - 30 : ");
+        System.out.print("Bilangan Ganjil dari 1 - 30 : " + '\n');
         for(int x=1; x <=30;x++){
         if(x % 2 == 1 )
         System.out.print(" "+x);
         }
         //Genap
-        System.out.print("Bilangan Genap dari 1 - 30 : ");
+        System.out.print('\n'+"Bilangan Genap dari 1 - 30 : " + '\n');
         for(int x=1; x <=30;x++){
         if(x % 2 == 0 )
         System.out.print(" "+x);
