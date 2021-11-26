b6839f8a90a32c6fde4e71237653f1517e3e0c93
alditiya <aldit4@gmail.com>
2021-11-26 12:07:21 +0700

    add looping project

---
+++ looping/GanjilGenap.java
@@ -0,0 +1,16 @@
+public class GanjilGenap {
+    public static void main(String [] args){
+        //Ganjil
+        System.out.print("Bilangan Ganjil dari 1 - 30 : ");
+        for(int x=1; x <=30;x++){
+        if(x % 2 == 1 )
+        System.out.print(" "+x);
+        }
+        //Genap
+        System.out.print("Bilangan Genap dari 1 - 30 : ");
+        for(int x=1; x <=30;x++){
+        if(x % 2 == 0 )
+        System.out.print(" "+x);
+        }
+    }
+}
